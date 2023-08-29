ff964e59ab0ceac413f20ae71a13064d225c8ca3
frontkick93 <zihui001@hotmail.com>
2023-08-29 16:24:21 +0800

    Your commit message here

---
+++ src/Myhome.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import React from "react";
+import axios from 'axios';
+
+function Myhome(){
+
+    const containerStyle = {
+        fontSize: '16px', // Adjust as needed
+        color: 'red' // Other styles
+    };
+
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Make an HTTP request to fetch data
+        axios.get('https://frontkick93.github.io/dataset/')
+        .then(response => {
+            const mydata = response.data;
+            console.log(response.data);
+            setData(mydata.query1);
+            setLoading(false);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            setError("Failed to fetch data.");
+            setLoading(false);
+        });
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    return (
+        <div style={containerStyle}>
+            {data.map(item => (
+                <li key={item.movementidd}>
+                    {item.movementidd}, {item.newmend}, {item.staffname2}
+                </li>
+            ))}
+        </div>
+    );
+}
+
+export default Myhome;
---
+++ src/Zoho.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Zoho(){
+    return(
+        
+        <div className="main2">
+                <iframe title="myframe" width="1860" height="880" src="https://analytics.zoho.com/open-view/2698918000000003845">hello</iframe>
+              
+       
+        </div>
+            
+        
+
+    )
+}
+export default Zoho;
